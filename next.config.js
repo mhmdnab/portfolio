b975355d5f73521c53cebbbad9134c41fd3b3fcb
mhmdnab <mhmdnab004@gmail.com>
2025-10-05 12:37:24 +0300

    deplyment on cloudflare 2

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
 
-module.exports = nextConfig;
-module.exports = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.unisplash.com",
-        port: "3000",
-        pathname:
-          "/photo-1550439062-609e1531270e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        hostname: "images.unsplash.com", // fixed typo: 'unisplash' → 'unsplash'
       },
     ],
   },
@@ -19,8 +15,10 @@ module.exports = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:5000/api/:path*", // Proxy to Backend
+        destination: "http://localhost:5000/api/:path*", // Proxy to backend
       },
     ];
   },
 };
+
+module.exports = nextConfig;
